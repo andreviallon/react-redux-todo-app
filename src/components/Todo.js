f33bbc658d0d9454872c6481e3aed1b89662811d
Andre Viallon <viallon.andre@gmail.com>
2018-09-29 12:21:51 +0200

    add todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -7,21 +7,39 @@ class Todo extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            todos: ['Clean Car', 'Do Laundry', 'Go grocery shopping']
+            todos: ['Clean Car', 'Do Laundry', 'Go grocery shopping'],
+            newTodo: ''
         }
+        this.onChange = this.onChange.bind(this);
+        this.onAddTodo = this.onAddTodo.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({ newTodo: [e.target.value] });
+    }
+
+    onAddTodo() {
+        const newTodo = this.state.newTodo;
+
+        newTodo.trim;
+
+        if (newTodo === '') return;
+
+        this.setState({ todos: [newTodo, ...this.state.todos] });
+        this.setState({ newTodo: '' });
     }
 
     render() {
         return (
             <div className="todo-container">
                 <h1 className="title-container">Todo List</h1>
-                <form className="form-container">
+                <div className="form-container">
                     <label>What would you like to do toady?</label>
                     <br />
                     <br />
-                    <input type="text" className="input" />
-                    <button>Submit</button>
-                </form>
+                    <input type="text" name="title" className="input" onChange={this.onChange} value={this.state.newTodo} />
+                    <button onClick={this.onAddTodo}>Submit</button>
+                </div>
                 <ul>{this.state.todos.map((todo, i) =>
                     <div key={i} className="flex-container">
                         <li>{todo}</li> <div className="fa fa-times"></div>
